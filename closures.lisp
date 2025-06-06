5f617db536a91bc1b2a64165593a88cbeed26723
Gleefre <varedif.a.s@gmail.com>
2025-06-06 21:21:50 +0200

    Do an early return

--- closures.lisp
+++ closures.lisp
@@ -201,7 +201,9 @@ against CHR-EXPR."
          (start-of-end-string-p (start-of-end-string-p str))
          (skip (skip str))
          (str (str str))
-         (chr (unless (zerop len) (schar str 0)))
+         (chr (if (zerop len) ;; empty STR is same as VOID
+                  (return-from create-matcher-aux next-fn)
+                  (schar str 0)))
          (end-string (and end-string (str end-string)))
          (end-string-len (if end-string
                            (length end-string)
@@ -239,9 +241,6 @@ against CHR-EXPR."
                               (*string*= end-string start-pos test-end-pos
                                          0 end-string-len)))
                      (funcall next-fn (+ start-pos len))))))
-          ((= len 0)
-            ;; empty STR is same as VOID
-            next-fn)
           (skip
             ;; a STR which can be skipped because some other function
             ;; has already confirmed that it matches
